bc29ff721fef29d5c1f0d857c5298e314dd7a349
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-07-19 17:22:25 +0100

    feat: whitelabelling

--- bridgetown/energy_tables/plugins/builders/suppliers_builder.rb
+++ bridgetown/energy_tables/plugins/builders/suppliers_builder.rb
@@ -3,6 +3,8 @@ module Builders
     def build
       hook :site, :post_read do
         site.data.supplier_data.each do |supplier|
+          next unless valid_supplier?(supplier)
+          
           slug = format_slug(supplier.supplierName)
 
           add_resource :suppliers, "#{slug}.md" do
@@ -10,9 +12,11 @@ module Builders
             id supplier.supplierId
             name supplier.supplierName
             rank supplier.supplierRank
-            ranked supplier.dataAvailable.downcase == "true"
             billing_info supplier.billingInformation
             slug from: ->{ format_slug(supplier.supplierName) }
+            ranked from: ->{ ranked?(supplier) }
+            whitelabelled from: ->{ whitelabelled?(supplier) }
+            whitelabel_name from: ->{ format_whitelabel_name(supplier) }
           end
         end 
       end
@@ -22,6 +26,28 @@ module Builders
 
     def format_slug(supplier_name)
       supplier_name.parameterize
+    end
+
+    def format_whitelabel_name(supplier)
+      whitelabel_supplier = site.data.supplier_data.select do |s| 
+        s.SupplierId == supplier.whilteLabelId
+      end.first
+
+      return if whitelabel_supplier.nil?
+
+      whitelabel_supplier.supplierName 
+    end
+
+    def ranked?(supplier)
+      supplier.dataAvailable.downcase == "true"
+    end
+
+    def whitelabelled?(supplier)
+      supplier.whilteLabelId != "0"
+    end
+
+    def valid_supplier?(supplier)
+      whitelabelled?(supplier) || ranked?(supplier)
     end
   end
 end
