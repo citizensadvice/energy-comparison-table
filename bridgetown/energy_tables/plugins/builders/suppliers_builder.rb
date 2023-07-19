08c85d475cdcb80263644e33a2f4b8670528b172
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-07-19 16:31:18 +0100

    feat: create suppliers from csv

--- bridgetown/energy_tables/plugins/builders/suppliers_builder.rb
+++ bridgetown/energy_tables/plugins/builders/suppliers_builder.rb
@@ -3,16 +3,25 @@ module Builders
     def build
       hook :site, :post_read do
         site.data.supplier_data.each do |supplier|
-          slug = supplier.supplierName.parameterize
+          slug = format_slug(supplier.supplierName)
+
           add_resource :suppliers, "#{slug}.md" do
+            layout "supplier"
+            id supplier.supplierId
             name supplier.supplierName
             rank supplier.supplierRank
-            billing_information supplier.billing_information
-            slug slug
-            content "<p>I am the content</p>"
+            ranked supplier.dataAvailable.downcase == "true"
+            billing_info supplier.billingInformation
+            slug from: ->{ format_slug(supplier.supplierName) }
           end
-        end
+        end 
       end
+    end
+
+    private
+
+    def format_slug(supplier_name)
+      supplier_name.parameterize
     end
   end
 end
