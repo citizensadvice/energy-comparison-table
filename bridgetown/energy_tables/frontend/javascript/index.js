2e4de9f0363883a8a92180a84a0bdc13d4b2b68a
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-07-20 14:00:31 +0100

    feat: add design system and webpack

--- bridgetown/energy_tables/frontend/javascript/index.js
+++ bridgetown/energy_tables/frontend/javascript/index.js
@@ -1,7 +1,3 @@
-import "index.css"
-import "syntax-highlighting.css"
+import "index.scss";
 
-// Import all JavaScript & CSS files from src/_components
-import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
-
-console.info("Bridgetown is loaded!")
+console.info("Bridgetown is loaded!");
