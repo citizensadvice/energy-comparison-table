47ee15ae2f757ef60a5a3c974131f1a1480ad836
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-08-08 17:47:33 +0100

    feat: example routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#index"
+  root "suppliers#index"
+
+  resources :suppliers, only: %i[index show]
 end
