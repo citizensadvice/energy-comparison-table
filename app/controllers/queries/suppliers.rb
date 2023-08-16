d466cb4e066b4c614ef514cd7e63effea375d03c
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-08-16 10:04:57 +0100

    refactor: make the client a class

--- app/controllers/queries/suppliers.rb
+++ app/controllers/queries/suppliers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Queries
-  Suppliers = Contentful::Graphql::Client.parse <<-GRAPHQL
+  Suppliers = Contentful::Graphql::Client.new.parse <<-GRAPHQL
     query {
       energySupplierCollection {
         items {
--- app/lib/contentful/graphql/client.rb
+++ app/lib/contentful/graphql/client.rb
@@ -2,8 +2,30 @@
 
 module Contentful
   module Graphql
-    HTTP = Contentful::Graphql::Adapter.new
-    SCHEMA = GraphQL::Client.load_schema("db/schema.json")
-    Client = GraphQL::Client.new(schema: SCHEMA, execute: HTTP)
+    class Client
+      attr_reader :http, :schema, :client
+
+      delegate :parse, to: :client
+
+      def initialize
+        @http = Contentful::Graphql::Adapter.new
+        @schema = load_schema
+        @client = GraphQL::Client.new(schema:, execute: http)
+      end
+
+      def query(query, variables: {}, context: {})
+        @client.query(query, variables:, context:)
+      end
+
+      private
+
+      def load_schema
+        if Feature.enabled? "LOAD_SCHEMA_DYNAMICALLY"
+          GraphQL::Client.load_schema(http)
+        else
+          GraphQL::Client.load_schema("db/schema.json")
+        end
+      end
+    end
   end
 end
