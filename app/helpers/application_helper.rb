0b0957400c2b923db1d6809ef45df12f6202e479
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-08-17 16:01:18 +0100

    feat: add header component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def scotland?
+    request.path.starts_with?("/scotland")
+  end
+
+  def wales?
+    request.path.starts_with?("/wales")
+  end
 end
