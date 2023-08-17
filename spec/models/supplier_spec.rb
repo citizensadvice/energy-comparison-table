2341259915587588067ee8b1e9d40891393a1dd5
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-08-17 15:51:02 +0100

    feat: rspecs for supplier model

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -4,21 +4,45 @@ require "rails_helper"
 require "pry"
 
 RSpec.describe(Supplier) do
-  describe "#fetch_all" do
-    subject(:all_suppliers) { described_class.fetch_all }
+  describe "class methods" do
+    describe "#fetch_all" do
+      subject(:all_suppliers) { described_class.fetch_all }
 
-    around do |example|
-      ClimateControl.modify(USE_TEST_SUPPLIERS: "true") do
-        VCR.use_cassette("supplier/fetch_all") do
-          example.run
+      around do |example|
+        ClimateControl.modify(USE_TEST_SUPPLIERS: "true") do
+          VCR.use_cassette("supplier/fetch_all") do
+            example.run
+          end
         end
       end
-    end
 
-    it { is_expected.to be_present }
+      it { is_expected.to be_present }
 
-    it "returns ranked suppliers in ascending rank order" do
-      expect(all_suppliers.first.rank < all_suppliers.second.rank).to be true
+      it "returns ranked suppliers in ascending rank order" do
+        expect(all_suppliers.first.rank < all_suppliers.second.rank).to be true
+      end
     end
+  end
+
+  describe "a ranked supplier" do
+    subject { build(:supplier, :ranked) }
+
+    its(:name) { is_expected.to eq "An Energy Supplier Inc" }
+    its(:slug) { is_expected.to eq "an-energy-supplier-inc" }
+    its(:rank) { is_expected.to eq 1 }
+    its(:previous_rank) { is_expected.to eq 2 }
+    its(:complaints_rating) { is_expected.to eq 4 }
+    its(:overall_rating) { is_expected.to eq 5 }
+    its(:id) { is_expected.to eq "an-energy-supplier-inc" }
+
+    it { is_expected.to be_persisted }
+    it { is_expected.to be_ranked }
+  end
+
+  describe "an unranked supplier" do
+    subject { build(:supplier) }
+
+    its(:rank) { is_expected.to be_nil }
+    it { is_expected.not_to be_ranked }
   end
 end
