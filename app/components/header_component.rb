5d8659591474fcf3f52a0425e59e910d1172d989
Lucy Stringer <62619601+stringiest@users.noreply.github.com>
2023-08-18 09:21:36 +0100

    feat: add header component for England & Wales (#57)
    * feat: add header component
    
    * test: add test for standard EW header components
    
    * fix: fix test following change to appease rubocop

---
+++ app/components/header_component.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class HeaderComponent < ViewComponent::Base
+  delegate :scotland?, to: :helpers
+
+  def call
+    render CitizensAdviceComponents::Header.new do |c|
+      c.with_logo(**logo)
+      c.with_search_form(search_action_url: search_url)
+    end
+  end
+
+  def logo
+    {
+      url: logo_url,
+      title: I18n.t("header.homepage_title")
+    }
+  end
+
+  def logo_url
+    if scotland?
+      "https://www.citizensadvice.org.uk/scotland"
+    else
+      "https://www.citizensadvice.org.uk"
+    end
+  end
+
+  def search_url
+    if scotland?
+      "https://www.citizensadvice.org.uk/scotland/resources-and-tools/search-navigation-tools/Search/"
+    else
+      "https://www.citizensadvice.org.uk/s"
+    end
+  end
+end
---
+++ spec/components/header_component_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe HeaderComponent, type: :component do
+  subject { page }
+
+  let(:component) { described_class.new }
+
+  describe "common elements" do
+    before { render_inline component }
+
+    it { is_expected.to have_link "Citizens Advice Homepage", href: "https://www.citizensadvice.org.uk" }
+    it { is_expected.to have_selector "[action='https://www.citizensadvice.org.uk/s']" }
+  end
+end
