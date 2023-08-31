5b6bc90964e34bab02eed9346790ab8d9672380e
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-08-31 10:48:42 +0100

    refactor: remove additional scotland layout and add logic into main layout instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,9 @@
 class ApplicationController < ActionController::Base
   include SwiftypeMeta
 
-  layout :layout_type
-
   def not_found
     render "errors/not_found", status: :not_found, format: :html
   end
 
   private
-
-  def layout_type
-    request.path.starts_with?("/scotland") ? "energy_comparison_table_scotland" : "application"
-  end
 end
