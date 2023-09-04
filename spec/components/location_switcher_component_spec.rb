d54a2fa7bbcf711324bb769c9ce28c76d77e6aef
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-09-04 17:02:49 +0100

    test: add tests for location switcher component

---
+++ spec/components/location_switcher_component_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe LocationSwitcherComponent, type: :component do
+  subject { page }
+
+  before do
+    render_inline described_class.new(
+      current_country:
+    )
+  end
+
+  context "when current country is england" do
+    let(:current_country) { "england" }
+
+    it { is_expected.to have_text "This advice applies to England" }
+    it { is_expected.to have_link "a", text: "See advice for Scotland" }
+    it { is_expected.to have_link "a", text: "See advice for Wales" }
+    it { is_expected.not_to have_link "a", text: "See advice for England" }
+  end
+
+  context "when current country is scotland" do
+    let(:current_country) { "scotland" }
+
+    it { is_expected.to have_text "This advice applies to Scotland" }
+    it { is_expected.to have_link "a", text: "See advice for England" }
+    it { is_expected.to have_link "a", text: "See advice for Wales" }
+    it { is_expected.not_to have_link "a", text: "See advice for Scotland" }
+  end
+
+  context "when current country is wales" do
+    let(:current_country) { "wales" }
+
+    it { is_expected.to have_text "This advice applies to Wales" }
+    it { is_expected.to have_link "a", text: "See advice for England" }
+    it { is_expected.to have_link "a", text: "See advice for Scotland" }
+    it { is_expected.not_to have_link "a", text: "See advice for Wales" }
+  end
+end
