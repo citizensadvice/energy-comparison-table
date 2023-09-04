2593a5e6bb6ee6bf77805a1bde50f3aec53c693e
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-04 11:03:28 +0100

    feat: -2 scores now render Not scored

--- app/components/score_component.rb
+++ app/components/score_component.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ScoreComponent < ViewComponent::Base
-  renders_one :link
+  NOT_SCORED = -2
 
-  def initialize(score:, highlight_stars: false)
+  def initialize(score:, show_decimal_score: false)
     @score = score
-    @highlight_stars = highlight_stars
+    @show_decimal_score = show_decimal_score
   end
 
   def render?
@@ -14,21 +14,30 @@ class ScoreComponent < ViewComponent::Base
 
   private
 
-  def highlight_stars?
-    @highlight_stars
+  def show_decimal_score?
+    @show_decimal_score
   end
 
-  def round_score?
-    !highlight_stars?
+  def highlight_stars?
+    # We only highlight the stars when a decimal score is displayed
+    show_decimal_score?
+  end
+
+  def scored?
+    @score != NOT_SCORED
   end
 
   def score_text
-    return "#{@score.round} out of 5" if round_score?
+    scored? ? score_out_of_five : "Not scored"
+  end
 
-    "#{@score.round(2)} out of 5"
+  def score_out_of_five
+    return "#{score_number} out of 5" unless show_decimal_score?
+
+    "#{score_number} out of 5"
   end
 
   def score_number
-    highlight_stars? ? @score.round(1) : @score.round
+    show_decimal_score? ? @score.round(1) : @score.round
   end
 end
--- spec/components/previews/score_component_preview.rb
+++ spec/components/previews/score_component_preview.rb
@@ -7,17 +7,22 @@ class ScoreComponentPreview < ViewComponent::Preview
   end
 
   def highlighted_stars
-    component = ScoreComponent.new(score: 4.4, highlight_stars: true)
+    component = ScoreComponent.new(score: 4.4, show_decimal_score: true)
     render component
   end
 
   def half_star
-    component = ScoreComponent.new(score: 3.8, highlight_stars: true)
+    component = ScoreComponent.new(score: 3.8, show_decimal_score: true)
+    render component
+  end
+
+  def not_scored
+    component = ScoreComponent.new(score: -2)
     render component
   end
 
   def with_link
-    component = ScoreComponent.new(score: 1, highlight_stars: true).with_content(tag.a("More details", href: "https://www.example.com"))
+    component = ScoreComponent.new(score: 1, show_decimal_score: true).with_content(tag.a("More details", href: "https://www.example.com"))
     render component
   end
 end
