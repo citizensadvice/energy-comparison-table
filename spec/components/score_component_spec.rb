8dab1eadb2d5954a133869d4b47e4e91bc4e0474
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-04 11:48:13 +0100

    test: spec for score component

--- spec/components/score_component_spec.rb
+++ spec/components/score_component_spec.rb
@@ -3,13 +3,55 @@
 require "rails_helper"
 
 RSpec.describe ScoreComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:score_component) { page }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:score) { 3 }
+  let(:show_decimal_score) { nil }
+
+  before do
+    render_inline described_class.new(score: score.presence, show_decimal_score: show_decimal_score.presence)
+  end
+
+  it { is_expected.to have_text "3 out of 5" }
+  it { is_expected.to have_selector ".stars" }
+  it { is_expected.not_to have_selector ".stars--highlight" }
+
+  context "when the score has a decimal place that is less than .5 but not shown" do
+    let(:score) { 3.4 }
+
+    it "rounds the score down correctly" do
+      expect(score_component).to have_text "3 out of 5"
+    end
+  end
+
+  context "when the score has a decimal place that is more than .5 but not shown" do
+    let(:score) { 3.8 }
+
+    it "rounds the score up correctly" do
+      expect(score_component).to have_text "4 out of 5"
+    end
+  end
+
+  context "when the 'not scored' score is provided" do
+    let(:score) { -2 }
+
+    it { is_expected.to have_text "Not scored" }
+    it { is_expected.not_to have_selector ".stars" }
+  end
+
+  context "when the score is shown as a decimal" do
+    let(:score) { 1.2 }
+    let(:show_decimal_score) { true }
+
+    it { is_expected.to have_text "1.2 out of 5" }
+    it { is_expected.to have_selector ".stars--highlight" }
+  end
+
+  context "when content is provided" do
+    before do
+      render_inline described_class.new(score: 2).with_content("Hello")
+    end
+
+    it { is_expected.to have_text("Hello") }
+  end
 end
