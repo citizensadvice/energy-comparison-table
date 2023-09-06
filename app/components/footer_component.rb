5ddf5ac58d7c9e26b97663e0de9258d10801cc9c
Lucy Stringer <62619601+stringiest@users.noreply.github.com>
2023-09-06 11:23:38 +0100

    Feat/footer (#73)
    * feat: interim commit adding footer for E/W/S. Need to change the way feedback_survey_id is inserted, as currently tests are failing
    
    * feat: remove scotland logo from footer, rename header css file to logos to match usage
    
    * refactor: change how columns and feedbacl_id are passed into footer component
    
    * refactor: remove unnecessary legal_summary references
    
    * refactor: update current_country method to newest version merged to main
    
    * refactor: move current_country into country helper
    
    * fix: fix missing wales prefix in header and footer links
    
    * fix: fix linting error
    
    * fix: reinstate missing header css file
    
    * fix: re-remove current_country from supplier controller, update set_swifttype_meta_tags to set audience filter to england when current_country is blank

---
+++ app/components/footer_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class FooterComponent < ViewComponent::Base
+  delegate :scotland?, :scotland_public_website_footer_nav_links, :public_website_footer_nav_links, to: :helpers
+
+  def initialize(current_path:, feedback_survey_id:, columns:, feedback_title: nil)
+    super
+    @current_path = current_path
+    @feedback_survey_id = feedback_survey_id
+    @feedback_title = feedback_title
+    @columns = columns
+  end
+
+  def call
+    render CitizensAdviceComponents::Footer.new do |c|
+      c.with_feedback_link(url: research_uri.to_s,
+                           title: @feedback_title,
+                           external: true,
+                           new_tab: true)
+      c.with_columns(@columns)
+    end
+  end
+
+  private
+
+  def research_uri
+    hash = { p: @current_path }
+
+    URI::HTTPS.build(host: "www.research.net", path: "/r/#{@feedback_survey_id}", query: hash.to_query)
+  end
+end
---
+++ app/helpers/footer_helper.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module FooterHelper
+  def public_website_footer_nav_links
+    [
+      {
+        title: "Advice",
+        links: navigation_links
+      },
+      {
+        title: "Resources and tools",
+        links: [
+          { title: "Adviser resources", url: "/about-us/adviser-resources/" },
+          { title: "Education resources", url: "/about-us/how-we-provide-advice/our-prevention-work/education/" },
+          { title: "Site search", url: "/s" },
+          { title: "A to Z of advice", url: "/resources-and-tools/search-navigation-tools/a-to-z-of-advice/" }
+        ]
+      },
+      {
+        title: "More from us",
+        links: [
+          { title: "About us", url: "/about-us/" },
+          { title: "Contact us", url: "/about-us/contact-us/contact-us/contact-us/" },
+          { title: "Support us", url: "/about-us/support-us/" },
+          { title: "Annual reports", url: "/about-us/our-work/annual-reports/" },
+          { title: "Complaints", url: "/about-us/contact-us/contact-us/make-a-complaint-about-us/" },
+          { title: "Media", url: "/about-us/about-us1/media/" },
+          { title: "Modern slavery statement", url: "/about-us/modern-slavery-statement/" },
+          { title: "Policy research", url: "/about-us/policy/" },
+          { title: "Volunteering", url: "/about-us/support-us/volunteering/" },
+          { title: "Jobs", url: "/about-us/job-and-voluntary-opportunities/" }
+        ]
+      },
+      {
+        title: "About this site",
+        links: [
+          { title: "Accessibility statement", url: "/resources-and-tools/about-this-site/accessibility/" },
+          { title: "Terms and conditions", url: "/resources-and-tools/about-this-site/terms-and-conditions/" },
+          { title: "Privacy and cookies", url: "/about-us/citizens-advice-privacy-policy/" }
+        ]
+      }
+    ]
+  end
+
+  def scotland_public_website_footer_nav_links
+    [
+      {
+        title: "Advice",
+        links: navigation_links
+      },
+      {
+        title: "Resources and tools",
+        links: [
+          { title: "Where to get advice", url: "/scotland/about-us/get-advice-s/" },
+          { title: "Template letters", url: "/scotland/consumer/template-letters/letters/" },
+          { title: "A to Z of advice", url: "/scotland/resources-and-tools/search-navigation-tools/a-to-z-of-advice/" }
+        ]
+      },
+      {
+        title: "More from us",
+        links: [
+          { title: "More help", url: "/scotland/about-us/" },
+          { title: "Complaints", url: "https://www.cas.org.uk/complaints" },
+          { title: "Campaigns", url: "https://www.cas.org.uk/campaigns" },
+          { title: "Publications", url: "https://www.cas.org.uk/publications" },
+          { title: "Support us", url: "https://www.cas.org.uk/about-us/support-our-work" },
+          { title: "Volunteering", url: "https://www.cas.org.uk/about-us/volunteer-citizens-advice-bureau" }
+        ]
+      },
+      {
+        title: "About this site",
+        links: [
+          { title: "Disclaimer and copyright", url: "/scotland/resources-and-tools/about-this-site/terms-and-conditions/" }
+        ]
+      }
+    ]
+  end
+end
