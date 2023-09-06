b31ce6e6400a7f3fe1cac01eda161da5da264f86
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-06 12:36:39 +0100

    feat: select funtionality and styles

--- app/components/input/select_component.rb
+++ app/components/input/select_component.rb
@@ -2,7 +2,7 @@
 
 module Input
   class SelectComponent < CitizensAdviceComponents::Input
-    attr_reader :base_input_args, :select_options
+    attr_reader :base_input_args, :select_options, :value
 
     def initialize(select_options:, **args)
       @select_options = select_options
@@ -10,11 +10,18 @@ module Input
       super(**@base_input_args)
     end
 
+    private
+
     def select_classes
       %w[
         select
         cads-input
       ]
+    end
+
+    def base_input_attributes
+      # selects do not control their current value with the `value` attribute
+      super.reject { |k| k == :value }
     end
   end
 end
