ee78ce51033b36b2d553e70647c3437bb64ce471
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-07 17:10:24 +0100

    feat: delete redundant previews

--- spec/components/previews/unranked_suppliers/details_component_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UnrankedSuppliers::DetailsComponentPreview < ViewComponent::Preview
-  def default
-    render(UnrankedSuppliers::DetailsComponent.new)
-  end
-end
