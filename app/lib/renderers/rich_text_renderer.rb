ccec8433a215ce00d58a0594de5defc6ecea2e4c
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-07 14:30:50 +0100

    feat: add br paragraph renderer
    By default, the contentful rich text renderer renders nothing when it encounters a new line.  We need it to render a <br> element, so that the billing info / opening hours etc render as per the designs.

--- app/lib/renderers/rich_text_renderer.rb
+++ app/lib/renderers/rich_text_renderer.rb
@@ -3,7 +3,12 @@
 module Renderers
   class RichTextRenderer < RichTextRenderer::Renderer
     def render_with_breaks(node)
+      return if node.blank?
+
+      # We can trust content from Contentful
+      # rubocop:disable Rails/OutputSafety
       render(node.json).gsub("\n", "<br/>").html_safe
+      # rubocop:enable Rails/OutputSafety
     end
   end
 end
