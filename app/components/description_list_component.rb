e593aefe2d96a897620e0749d89655f7c034d119
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-07 14:31:52 +0100

    feat: add a basic description list component

---
+++ app/components/description_list_component.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "pry"
+
+class DescriptionListComponent < ViewComponent::Base
+  renders_one :title
+  renders_many :descriptions, "DescriptionComponent"
+
+  def render?
+    descriptions?
+  end
+
+  class DescriptionComponent < ViewComponent::Base
+    attr_reader :term, :description
+
+    def initialize(term:, description:)
+      @term = term
+      @description = description
+    end
+  end
+end
