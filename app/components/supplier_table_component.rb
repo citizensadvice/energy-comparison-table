7e29d892bd9c19ea248d09d62a0ea139c6e8bf5c
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-12 16:15:57 +0100

    feat: add highlighting param to view and component

--- app/components/supplier_table_component.rb
+++ app/components/supplier_table_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class SupplierTableComponent < ViewComponent::Base
-  attr_reader :suppliers
+  attr_reader :suppliers, :highlight_supplier_with_slug
 
-  def initialize(suppliers)
+  def initialize(suppliers, highlight_supplier_with_slug: nil)
     @suppliers = suppliers
+    @highlight_supplier_with_slug = highlight_supplier_with_slug
   end
 end
