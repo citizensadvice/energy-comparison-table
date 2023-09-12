60d3bb295b557ff3b017946f424b7e3a9e41cf4f
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-12 16:15:25 +0100

    feat: add highlighting to supplier table row

--- spec/components/supplier_table_row_component_spec.rb
+++ spec/components/supplier_table_row_component_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe SupplierTableRowComponent, type: :component do
   it { is_expected.to have_selector ".stars", count: 4 }
   it { is_expected.to have_selector ".stars--highlight", count: 1 }
   it { is_expected.to have_link "More details", href: "#{CSR_APP_PATH}an-energy-supplier-inc/details" }
+
+  context "when the row is highlighted" do
+    before do
+      render_inline described_class.new(supplier, highlight: true)
+    end
+
+    it { is_expected.to have_selector ".supplier-table__row--highlight" }
+
+    context "when the supplier is not top 3" do
+      let(:supplier) { build(:supplier, :low_ranking) }
+
+      it { is_expected.to have_selector ".supplier-table__row--top-border" }
+    end
+  end
 end
