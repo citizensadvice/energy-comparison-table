884be4b5ae77502d7a182831474aa708a971e95e
Zhelyan Panchev <zhelyan.panchev@citizensadvice.org.uk>
2023-09-13 14:52:32 +0100

    allow cab-prod2 to pull images

--- infrastructure/cdk/app.py
+++ infrastructure/cdk/app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
+from aws_cdk.aws_iam import AccountPrincipal
 
 from stacks.ecr import EcrRepository
 
 app = cdk.App()
 
+dev_acount = app.node.get_context("dev")["AWS_ACCOUNT"]
+prod_acount = app.node.get_context("prod")["AWS_ACCOUNT"]
+
 EcrRepository(
     app,
     "EnergyComparisonTableEcrRepo",
-    # cita-devops
-    env=cdk.Environment(
-        account=app.node.get_context("dev")["AWS_ACCOUNT"], region="eu-west-1"
-    ),
+    env=cdk.Environment(account=dev_acount, region="eu-west-1"),
+    pull_principals=[AccountPrincipal(prod_acount)],
     tags={"Environment": "prod"},
 )
 
--- infrastructure/cdk/stacks/ecr.py
+++ infrastructure/cdk/stacks/ecr.py
@@ -1,13 +1,21 @@
+from typing import Sequence
 from aws_cdk import Duration, RemovalPolicy, Stack
 from aws_cdk.aws_ecr import LifecycleRule, Repository, TagStatus
+from aws_cdk.aws_iam import IPrincipal
 from constructs import Construct
 
 
 class EcrRepository(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        pull_principals: Sequence[IPrincipal] = [],
+        **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
-        Repository(
+        repo = Repository(
             self,
             "Default",
             removal_policy=RemovalPolicy.DESTROY,
@@ -29,3 +37,5 @@ class EcrRepository(Stack):
                 ),
             ],
         )
+        for principal in pull_principals:
+            repo.grant_pull(principal)
