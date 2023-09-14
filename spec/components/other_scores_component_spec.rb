3c98312f4735aa1230978e4daaf83fa1a9d12713
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-14 09:30:22 +0100

    test: add spec for component

--- spec/components/other_scores_component_spec.rb
+++ spec/components/other_scores_component_spec.rb
@@ -3,13 +3,33 @@
 require "rails_helper"
 
 RSpec.describe OtherScoresComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { page }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:supplier) { build(:supplier, :ranked) }
+
+  before do
+    render_inline described_class.new(supplier)
+  end
+
+  it { is_expected.to have_text "Other scores" }
+
+  # contact ratings
+  it { is_expected.to have_text "Emails responded to within 2 days" }
+  it { is_expected.to have_text "89%" }
+  it { is_expected.to have_text "Average call centre wait time (hours:minutes)" }
+  it { is_expected.to have_text "03:27" }
+
+  # complaints ratings
+  it { is_expected.to have_text "Complaints to third parties" }
+  it { is_expected.to have_text "172 per 10,000 customers" }
+
+  # bills ratings
+  it { is_expected.to have_text "Customers who had an accurate bill at least once a year" }
+  it { is_expected.to have_text "99%" }
+
+  context "when there is no supplier" do
+    let(:supplier) { nil }
+
+    it { is_expected.not_to have_selector "body" }
+  end
 end
