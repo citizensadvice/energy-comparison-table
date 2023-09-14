a969d6d6a643b27d021d1de462765067694eaeaa
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-14 12:44:07 +0100

    test: spec for other info component

---
+++ spec/components/other_info_component_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe OtherInfoComponent, type: :component do
+  subject { page }
+
+  let(:supplier) { build(:supplier, :ranked) }
+
+  before do
+    render_inline described_class.new(supplier)
+  end
+
+  it { is_expected.to have_text "Fuel Mix" }
+  it { is_expected.to have_text "fuel mix content" }
+
+  it { is_expected.to have_text "Opening Hours" }
+  it { is_expected.to have_text "opening hours content" }
+
+  it { is_expected.to have_text "Contact Information" }
+  it { is_expected.to have_text "some contact details" }
+
+  it { is_expected.to have_link "hello@example.com", href: "mailto:hello@example.com" }
+end
