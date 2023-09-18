d07e420d7eeafdc3c66c3a09c7d9c8bf843fe787
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-18 15:03:31 +0100

    feat: add js and styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
-import initExample from "./example";
+import initSupplierTableButton from "./supplier-table";
 
-initExample();
+try {
+  initSupplierTableButton();
+} catch (error) {
+  document.querySelector("html").classList.add("no-js");
+  throw error;
+}
