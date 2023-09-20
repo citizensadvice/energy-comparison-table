00174792537ca1c13da0d07f8edfe19595136bcc
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-20 12:34:39 +0100

    feat: turn on digest in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  # Allow digest to be used in prod
+  config.assets.digest = true
+  config.public_file_server.enabled = true
+
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
