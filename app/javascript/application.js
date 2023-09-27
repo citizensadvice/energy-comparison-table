d237097f82e07a7d7af4032e59e4b811cbfc53d7
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-09-27 10:37:53 +0100

    feat: add greedy nav from CitA Design System

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Entry point for the build script in your package.json
+import initGreedyNav from "./greedy-nav";
 import initSupplierTableButton from "./supplier-table";
 
 try {
+  initGreedyNav();
   initSupplierTableButton();
 } catch (error) {
   document.querySelector("html").classList.add("no-js");
