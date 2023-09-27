f1d8724f96ce7abdf7084935ba0a3e892679b95f
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-09-27 11:30:00 +0100

    feat: add Design System js to header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Entry point for the build script in your package.json
 import initGreedyNav from "./greedy-nav";
 import initSupplierTableButton from "./supplier-table";
+import initHeader from '@citizensadvice/design-system/lib/header';
 
 try {
   initGreedyNav();
   initSupplierTableButton();
+  initHeader();
 } catch (error) {
   document.querySelector("html").classList.add("no-js");
   throw error;
