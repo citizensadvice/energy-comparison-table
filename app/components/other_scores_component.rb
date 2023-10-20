9b1c1e866c4e8e9839365c3f2091d706819be9a7
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-10-20 18:38:14 +0100

    refactor: abstract out separate hashes that comprise #descriptions, to fix linting error

--- app/components/other_scores_component.rb
+++ app/components/other_scores_component.rb
@@ -16,27 +16,47 @@ class OtherScoresComponent < ViewComponent::Base
 
   def descriptions
     [
-      {
-        term: content_tag(:h3, "Customer guarantees"),
-        description: content_tag(:p, guarantee_list)
-      },
-      {
-        term: content_tag(:h3, "Complaints to Citizens Advice and the Energy Ombudsman"),
-        description: content_tag(:p, "#{supplier.complaints_number} per 10,000 customers")
-      },
-      {
-        term: content_tag(:h3, "Average call centre wait time (minutes and seconds)"),
-        description: content_tag(:p, supplier.contact_time)
-      },
-      {
-        term: content_tag(:h3, "Average response time to social media messages (hours and minutes)"),
-        description: content_tag(:p, supplier.contact_social_media)
-      },
-      {
-        term: content_tag(:h3, "Emails responded to within 2 days"),
-        description: content_tag(:p, "#{supplier.contact_email}%")
-      }
+      guarantee_list_hash,
+      complaints_number_hash,
+      contact_time_hash,
+      contact_social_media_hash,
+      contact_email_hash
     ]
+  end
+
+  def guarantee_list_hash
+    {
+      term: content_tag(:h3, "Customer guarantees"),
+      description: content_tag(:p, guarantee_list)
+    }
+  end
+
+  def complaints_number_hash
+    {
+      term: content_tag(:h3, "Complaints to Citizens Advice and the Energy Ombudsman"),
+      description: content_tag(:p, "#{supplier.complaints_number} per 10,000 customers")
+    }
+  end
+
+  def contact_time_hash
+    {
+      term: content_tag(:h3, "Average call centre wait time (minutes and seconds)"),
+      description: content_tag(:p, supplier.contact_time)
+    }
+  end
+
+  def contact_social_media_hash
+    {
+      term: content_tag(:h3, "Average response time to social media messages (hours and minutes)"),
+      description: content_tag(:p, supplier.contact_social_media)
+    }
+  end
+
+  def contact_email_hash
+    {
+      term: content_tag(:h3, "Emails responded to within 2 days"),
+      description: content_tag(:p, "#{supplier.contact_email}%")
+    }
   end
 
   def guarantee_list
