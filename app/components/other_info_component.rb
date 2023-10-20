3d979e148be01ccbe52e5a7e018e458a30e9b4e7
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-10-20 16:31:32 +0100

    feat: add other contact info (ways to contact) field

--- app/components/other_info_component.rb
+++ app/components/other_info_component.rb
@@ -27,6 +27,14 @@ class OtherInfoComponent < ViewComponent::Base
       {
         term: content_tag(:h3, "Opening Hours"),
         description: opening_hours
+      },
+      {
+        term: content_tag(:h3, "Ways to contact"),
+        description: other_contact_info
+      },
+      {
+        term: content_tag(:h3, "Payment Information"),
+        description: billing_info
       }
     ].reject { |desc| desc[:description].blank? }
   end
@@ -35,11 +43,19 @@ class OtherInfoComponent < ViewComponent::Base
     renderer.render_with_breaks(supplier.contact_info)
   end
 
+  def other_contact_info
+    renderer.render_with_breaks(supplier.other_contact_info)
+  end
+
   def opening_hours
     renderer.render_with_breaks(supplier.opening_hours)
   end
 
   def fuel_mix
     renderer.render_without_breaks(supplier.fuel_mix)
+  end
+
+  def billing_info
+    renderer.render_without_breaks(supplier.billing_info)
   end
 end
--- app/components/other_scores_component.rb
+++ app/components/other_scores_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class OtherScoresComponent < ViewComponent::Base
-  attr_reader :supplier
+  attr_reader :supplier, :renderer
 
   delegate :scores_fragment, to: :helpers
 
   def initialize(supplier)
     @supplier = supplier
+    @renderer = Renderers::RichTextRenderer.new
   end
 
   def render?
@@ -17,20 +18,28 @@ class OtherScoresComponent < ViewComponent::Base
     [
       {
         term: content_tag(:h3, "Customer guarantees"),
-        description: content_tag(:p, "#{supplier.guarantee_list}%")
-      },
-      {
-        term: content_tag(:h3, "Emails responded to within 2 days"),
-        description: content_tag(:p, "#{supplier.contact_email}%")
+        description: content_tag(:p, guarantee_list)
       },
       {
         term: content_tag(:h3, "Complaints to Citizens Advice and the Energy Ombudsman"),
         description: content_tag(:p, "#{supplier.complaints_number} per 10,000 customers")
       },
       {
-        term: content_tag(:h3, "Average call centre wait time (minutes:seconds)"),
+        term: content_tag(:h3, "Average call centre wait time (minutes and seconds)"),
         description: content_tag(:p, supplier.contact_time)
+      },
+      {
+        term: content_tag(:h3, "Average response time to social media messages (hours and minutes)"),
+        description: content_tag(:p, supplier.contact_social_media)
+      },
+      {
+        term: content_tag(:h3, "Emails responded to within 2 days"),
+        description: content_tag(:p, "#{supplier.contact_email}%")
       }
     ]
+  end
+
+  def guarantee_list
+    renderer.render_with_breaks(supplier.guarantee_list)
   end
 end
