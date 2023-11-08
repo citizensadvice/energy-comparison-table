c9eb391a2fad380619f208a1fb506d3ab5f8a580
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-11-08 10:40:37 +0000

    test: try extending capybara wait_time to see if that allows css to load & test to pass

--- features/support/env.rb
+++ features/support/env.rb
@@ -59,3 +59,5 @@ ActionController::Base.allow_rescue = false
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 # Cucumber::Rails::Database.javascript_strategy = :truncation
+
+Capybara.default_max_wait_time = 5
