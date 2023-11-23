3b6dec432647c9f8cc7ebaeab8cc61b5bd5a13dd
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-11-23 17:34:27 +0000

    add steps for first scenario & define data-testid tag for supplier row

---
+++ features/step_definitions/supplier_table_steps.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+Given("I am on the Energy Supplier Table page") do
+  visit "/"
+end
+
+Then("I should see a table of ranked energy suppliers") do
+  expect(page).to have_css "[data-testid='supplier-table']"
+end
+
+And("I select a supplier from the table") do
+  big_energy_inc = find("[data-testid='supplier-big-energy-inc']")
+  within(big_energy_inc) do
+    click_on "More details"
+  end
+end
+
+Then("I am taken to the supplier detail page") do
+  expect(page).to have_current_path("#{CSR_APP_PATH}big-energy-inc/details")
+end
