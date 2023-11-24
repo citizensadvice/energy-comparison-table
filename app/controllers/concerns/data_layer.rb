acaaf7d63d1fa76b5c5877d53291094b4770deae
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-11-24 17:04:43 +0000

    feat: add dataLayer default properties

---
+++ app/controllers/concerns/data_layer.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module DataLayer
+  extend ActiveSupport::Concern
+
+  included do
+    def data_layer_properties
+      {
+        platform: "content-platform",
+        siteType: "Public Website",
+        pageTemplate: "Customer Service Ratings Page",
+        pageType: "Customer Service Ratings Page",
+        # language confusingly represents the current country: England, Wales etc.
+        # but needs to have this name to match up with Episerver page data.
+        language: @page.try(:current_country).to_s.capitalize
+      }
+    end
+    helper_method :data_layer_properties
+  end
+end
