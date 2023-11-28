5971f3310f21b442a01372f850ccf20262b84791
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-11-28 11:10:41 +0000

    amend path for file import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import initHeader from '@citizensadvice/design-system/lib/header';
 import initGreedyNav from "./greedy-nav";
 import initSupplierTableButton from "./supplier-table";
-import initSupplierTableTracking from "./supplier-table-tracking";
+import initSupplierTableTracking from "./supplier-table-tracking.ts";
 
 try {
   // Initialise design-system modules first
