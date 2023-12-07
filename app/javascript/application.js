de0c504aa11852f77a3438d506a7d4645fb4d289
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-12-07 15:05:11 +0000

    remove custom event for supplier table tracking

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import initHeader from '@citizensadvice/design-system/lib/header';
 import initGreedyNav from "./greedy-nav";
 import initSupplierTableButton from "./supplier-table";
-import initSupplierTableTracking from "./supplier-table-tracking.ts";
 
 try {
   // Initialise design-system modules first
@@ -11,9 +10,6 @@ try {
 
   // Initialise application modules
   initSupplierTableButton();
-
-  // Initialise tracking modules
-  initSupplierTableTracking();
 } catch (error) {
   document.querySelector("html").classList.add("no-js");
   throw error;
