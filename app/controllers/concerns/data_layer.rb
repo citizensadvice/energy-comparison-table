6d9b6a593872c2077fe8ce4783013458b8e38ebc
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2023-12-08 11:10:27 +0000

    feat: add conditional pageType and pageTemplate for supplier index and detail pages

--- app/controllers/concerns/data_layer.rb
+++ app/controllers/concerns/data_layer.rb
@@ -5,16 +5,26 @@ module DataLayer
 
   included do
     def data_layer_properties
-      {
-        platform: "content-platform",
-        siteType: "Public Website",
-        pageTemplate: "Customer Service Ratings Page",
-        pageType: "Customer Service Ratings Page",
-        # language confusingly represents the current country: England, Wales etc.
-        # but needs to have this name to match up with Episerver page data.
-        language: helpers.current_country.to_s.capitalize
-      }
+      default_data_layer_properties
+        .merge(custom_data_layer_properties.to_h)
+        .compact
     end
     helper_method :data_layer_properties
+  end
+
+  protected
+
+  def custom_data_layer_properties
+    {} # page-specific data added at controller level
+  end
+
+  def default_data_layer_properties
+    {
+      platform: "content-platform",
+      siteType: "Public Website",
+      # language confusingly represents the current country: England, Wales etc.
+      # but needs to have this name to match up with Episerver page data.
+      language: helpers.current_country.to_s.capitalize
+    }
   end
 end
