323a0683ed85e4b01814e6e0a3a5bc476f58432c
Lucy Stringer <lucy.stringer@citizensadvice.org.uk>
2024-07-04 09:40:44 +0100

    Feat: sort small suppliers dropdown alphabetically

--- spec/components/unranked_suppliers/dropdown_component_spec.rb
+++ spec/components/unranked_suppliers/dropdown_component_spec.rb
@@ -5,7 +5,16 @@ require "rails_helper"
 RSpec.describe UnrankedSuppliers::DropdownComponent, type: :component do
   subject { page }
 
-  let(:suppliers) { build_list(:supplier, 10) }
+  # let(:suppliers) { build_list(:supplier, 10) }
+  let(:suppliers) do
+    build_list(:supplier, 10) do |record, i|
+      # generate unique suppliers by adding a suffix letter
+      # then shuffle out of alphabetical order
+      record.data.name = "An Energy Supplier Inc #{(i + 65).chr}"
+      record.data.slug = "an-energy-supplier-inc-#{(i + 97).chr}"
+      record.data.save!
+    end.shuffle!
+  end
   let(:chosen_supplier_slug) { nil }
 
   before do
@@ -15,12 +24,13 @@ RSpec.describe UnrankedSuppliers::DropdownComponent, type: :component do
   it { is_expected.to have_text "Find your supplier" }
   it { is_expected.to have_css "option", count: 11 }
   it { is_expected.to have_text "Please select" }
+  it { is_expected.to have_text alphabetized_suppliers }
   it { is_expected.to have_no_css "option[selected]" }
 
   context "when a chosen supplier slug is passed" do
-    let(:chosen_supplier_slug) { "an-energy-supplier-inc" }
+    let(:chosen_supplier_slug) { "an-energy-supplier-inc-a" }
 
-    it { is_expected.to have_css "option[value*=an-energy-supplier-inc]" }
+    it { is_expected.to have_css "option[value*=an-energy-supplier-inc-a]" }
     it { is_expected.to have_css "option[selected]" }
   end
 
@@ -28,5 +38,11 @@ RSpec.describe UnrankedSuppliers::DropdownComponent, type: :component do
     let(:suppliers) { nil }
 
     it { is_expected.to have_no_text "Please select" }
+  end
+
+  private
+
+  def alphabetized_suppliers
+    "An Energy Supplier Inc A\nAn Energy Supplier Inc B\nAn Energy Supplier Inc C"
   end
 end
