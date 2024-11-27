f0c9a026841a4a5305dde979b93a686426cb7eed
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-11-27 15:17:55 +0000

    feat: add no-store cache header to all appliance calculator responses

--- app/controllers/appliance_calculator_controller.rb
+++ app/controllers/appliance_calculator_controller.rb
@@ -3,5 +3,13 @@
 class ApplianceCalculatorController < ApplicationController
   layout "appliance_calculator"
 
+  after_action :set_headers
+
   def index; end
+
+  private
+
+  def set_headers
+    response.headers["cache-control"] = "no-store"
+  end
 end
