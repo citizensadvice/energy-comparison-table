4e4fc0f515079148ffc5de6bdf70f714dbcd1f99
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-03 12:10:10 +0000

    feat: add validation to appliance step

--- app/models/daily_usage_creation/steps/appliance.rb
+++ app/models/daily_usage_creation/steps/appliance.rb
@@ -3,8 +3,16 @@
 module DailyUsageCreation
   module Steps
     class Appliance < WizardSteps::Step
-      attribute :id, :string
-      attribute :quantity, :integer
+      attribute :appliance_id, :string
+      attribute :quantity, :integer, default: 1
+
+      validates :appliance_id, presence: { message: "Select an appliance from the list" }
+      validates :quantity, presence: { message: "Enter a number greater than 0, for example 1" },
+                           numericality: { greater_than: 0, message: "Enter a number greater than 0, for example 1" }
+
+      def appliances
+        ::Appliance.fetch_all
+      end
     end
   end
 end
