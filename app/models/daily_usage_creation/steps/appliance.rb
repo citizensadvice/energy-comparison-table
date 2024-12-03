e7685ff3cddee79ee4918c0f15293d71c68b4eba
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-03 14:13:54 +0000

    feat: add error summary

--- app/models/daily_usage_creation/steps/appliance.rb
+++ app/models/daily_usage_creation/steps/appliance.rb
@@ -7,8 +7,8 @@ module DailyUsageCreation
       attribute :quantity, :integer, default: 1
 
       validates :appliance_id, presence: { message: "Select an appliance from the list" }
-      validates :quantity, presence: { message: "Enter a number greater than 0, for example 1" },
-                           numericality: { greater_than: 0, message: "Enter a number greater than 0, for example 1" }
+      validates :quantity, presence: { message: "Enter a number of appliances greater than 0, for example 1" },
+                           numericality: { greater_than: 0, message: "Enter a number of appliances greater than 0, for example 1" }
 
       def appliances
         ::Appliance.fetch_all
