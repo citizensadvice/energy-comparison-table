b4fac4f1869a3fe31176d403108747ebcd81d995
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-04 12:00:23 +0000

    feat: rename assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@citizensadvice/design-system/assets/")
-Rails.application.config.assets.prefix = "/energy-csr-assets"
+Rails.application.config.assets.prefix = "/energy-apps-assets"
