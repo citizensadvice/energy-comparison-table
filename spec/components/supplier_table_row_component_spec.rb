af64819f8e8b6cc8ec79586700d68e9a1eb5d94f
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-04 12:06:15 +0000

    fix: remove pry from spec files, add to spec_helper

--- spec/components/supplier_table_row_component_spec.rb
+++ spec/components/supplier_table_row_component_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "pry"
 
 RSpec.describe SupplierTableRowComponent, type: :component do
   subject { page }
--- spec/models/daily_usage_creation/steps/appliance_spec.rb
+++ spec/models/daily_usage_creation/steps/appliance_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "pry"
 
 RSpec.describe DailyUsageCreation::Steps::Appliance do
   describe "valid?" do
