31a222b3be0eb6d774a05012589e1867d9e0d2a0
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-05 09:47:09 +0000

    feat: use date component for time based usage question

--- app/models/daily_usage_creation/steps/time_usage.rb
+++ app/models/daily_usage_creation/steps/time_usage.rb
@@ -5,9 +5,25 @@ module DailyUsageCreation
     class TimeUsage < WizardSteps::Step
       attribute :hours, :integer
       attribute :minutes, :integer
+      attribute :frequency, :string
+
+      def label
+        "On average, how often do you use #{added_applicance_name}(s)?"
+      end
+
+      def frequency_options
+        [
+          FormOption.new(text: "Day", value: :daily),
+          FormOption.new(text: "Week", value: :weekly)
+        ]
+      end
 
       def skipped?
         @store[:cyclical?]
+      end
+
+      def added_applicance_name
+        @store[:added_appliance].dig("data", "name") || "appliance"
       end
     end
   end
