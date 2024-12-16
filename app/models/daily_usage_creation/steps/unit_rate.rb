b1cab40f97ffe84aa0ba075d2e9505ebe8260033
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-16 13:20:48 +0000

    feat: add the unit rate step

--- app/models/daily_usage_creation/steps/unit_rate.rb
+++ app/models/daily_usage_creation/steps/unit_rate.rb
@@ -3,7 +3,24 @@
 module DailyUsageCreation
   module Steps
     class UnitRate < WizardSteps::Step
-      attribute :unit_rate, :decimal
+      DEFAULT_UNIT_RATE = 24.5
+
+      attribute :unit_rate, :decimal, default: DEFAULT_UNIT_RATE
+
+      validates :unit_rate, presence: { message: "Enter a unit rate in pence per kilowatt hour" },
+                            numericality: {
+                              greater_than: 0,
+                              message: "Enter a unit rate in pence per kilowatt hour"
+                            }
+
+      def label
+        "The national average rate is #{DEFAULT_UNIT_RATE}p/kWh. You can change it to your rate if you know it.
+If you don't know your rate, we'll use the national average of #{DEFAULT_UNIT_RATE}p per kilowatt hour (kWh)"
+      end
+
+      def skipped?
+        @store[:unit_rate].present?
+      end
     end
   end
 end
