489b44a4869456966727b1c5a2119f1e910a59bb
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-12-16 08:35:33 +0000

    feat: add time usage step

--- app/models/daily_usage_creation/steps/time_usage.rb
+++ app/models/daily_usage_creation/steps/time_usage.rb
@@ -3,12 +3,18 @@
 module DailyUsageCreation
   module Steps
     class TimeUsage < WizardSteps::Step
+      include ActionView::Helpers::TagHelper
+
       attribute :hours, :integer
       attribute :minutes, :integer
       attribute :frequency, :string
 
+      validates :frequency, presence: { message: "Select a frequency from the list" }
+
+      validates_with Validators::TimeUsageValidator
+
       def label
-        "On average, how often do you use #{added_applicance_name}(s)?"
+        "On average, how often do you use #{added_appliance_name}(s)?"
       end
 
       def frequency_options
@@ -22,8 +28,18 @@ module DailyUsageCreation
         @store[:cyclical?]
       end
 
-      def added_applicance_name
+      def added_appliance_name
         @store[:added_appliance].dig("data", "name") || "appliance"
+      end
+
+      def error
+        errors[:base].first
+      end
+
+      def form_field_classes
+        classes = ["cads-form-field"]
+        classes << "cads-form-field--has-error" if error.present?
+        classes
       end
     end
   end
